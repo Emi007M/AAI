6fc36e981c7945d99609baf2a08987abfa2eca06
Koiek2 <koi2@wp.pl>
2016-04-08 17:17:08 +0200

    Revert "Goal_think, wandering in goals, desirability"
    This reverts commit e87d5c37d2d885b043c522cdd5b882e24f6d0c58.

--- Game/Goals/Goal_Think.cs
+++ Game/Goals/Goal_Think.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MathNet.Numerics.LinearAlgebra;
-using System.Windows;
 
 namespace Game.Goals
 {
     class Goal_Think : CompositeGoal
     {
-        //desirabilities
-        double stopAtPond = 0;
-        double goHome = 0;
-        double stopAtStone = 0;
-        double explore = 10;
+        
 
         public Goal_Think(MovingEntity p) : base(p)
         {
@@ -26,58 +20,13 @@ namespace Game.Goals
         {
             status = (int)Status.active;
 
-            stopAtPond = CalculateWater();
-            stopAtStone = CalculateStone();
-            //goHome = CalculateGoHome();
-
-            switch (SelectMostDiserable())
-            {
-                case 0:
-                    AddGoal_StopAtClosestWater();
-                    break;
-                case 1:
-                    AddGoal_StopAtClosestStone();
-                    break;
-                case 2:
-                    AddGoal_Explore();
-                    break;
-                case 3:
-                    AddGoal_GoBackToBase();
-                    break;
-
-            }
-
+          //  AddGoal_GoBackToBase();
+           // AddGoal_FollowPath(new System.Windows.Vector(300, 500));
+          //  AddGoal_FindClosestWater();
+           //
         }
 
-        public int CalculateWater()
-        {
-            //  Vector ownerC = new Vector(owner.location.X, owner.location.Y);
-            foreach (Pond p in owner.gw.collecting.ponds)
-            {
-                // Vector p_center = new Vector(p.location.X, p.location.Y);
-                double lenght = Math.Sqrt((Math.Pow(owner.getX() - p.location.X, 2)) + Math.Pow(owner.getY() - p.location.Y, 2));
-                if (lenght < 2 * p.r)
-                {
-                    return 100 * owner.gw.soldiers.Count - 80 * owner.gw.collecting.waterAmount; //- x*owner.gw.castle.waterAmount
-                }
-            }
-            return 0;
-        }
 
-        public int CalculateStone()
-        {
-
-            foreach (Stone s in owner.gw.collecting.stones)
-            {
-                // Vector p_center = new Vector(p.location.X, p.location.Y);
-                double lenght = Math.Sqrt((Math.Pow(owner.getX() - s.location.X, 2)) + Math.Pow(owner.getY() - s.location.Y, 2));
-                if (lenght < 2 * s.r)
-                {
-                    return 99 * owner.gw.soldiers.Count - 80 * owner.gw.collecting.stoneAmount; //- x*owner.gw.castle.waterAmount
-                }
-            }
-            return 0;
-        }
         public override int Process()
         {
             if (!isActive()) Activate();
@@ -87,16 +36,6 @@ namespace Game.Goals
             return status;
         }
 
-        public int SelectMostDiserable()
-        {
-
-            double[] desirability = { stopAtPond, stopAtStone, explore, goHome };
-
-            return desirability.ToList().IndexOf(desirability.Max()); //returns 0 for pond,        1 for stone,      2 for explore,          3 for goHome
-
-
-            
-        }
         public override void Terminate()
         {
             //
@@ -106,7 +45,7 @@ namespace Game.Goals
 
         public void AddGoal_Explore()
         {
-            owner.gw.soldiers.ElementAt(0).useExplore();
+            //
         }
 
         public void AddGoal_FollowPath(System.Windows.Vector target)
@@ -123,7 +62,7 @@ namespace Game.Goals
             AddSubgoal(new Goal_FollowPath(owner, new System.Windows.Vector(750.0,320.0)));
         }
 
-        internal void AddGoal_StopAtClosestWater()
+        internal void AddGoal_FindClosestWater()
         {
 
             int start = owner.gw.grid.getVertex((int)owner.getX(), (int)owner.getY());
@@ -139,10 +78,9 @@ namespace Game.Goals
                 m.useLeaderFollow(owner);
             }
         }
-    
 
 
-        internal void AddGoal_StopAtClosestStone()
+        internal void AddGoal_FindClosestStone()
         {
 
             int start = owner.gw.grid.getVertex((int)owner.getX(), (int)owner.getY());
