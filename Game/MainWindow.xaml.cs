4b504ba0ec5dfbdda64350f2c0ea2b4512843bfa
Emilia <emi.miod@gmail.com>
2016-04-09 12:27:30 +0200

    labels for water, stones

--- Game/MainWindow.xaml.cs
+++ Game/MainWindow.xaml.cs
@@ -185,11 +185,13 @@ namespace Game
                 }
                 {//castle
                     Label txt = (Label)gw.castle.image.Children[2];
-                    txt.Content = " water: " + gw.castle.WaterAmount + "/" + gw.castle.getWaterCapacity();
+                    txt.Content = "   Lvl " + gw.castle.lvl;
+                    txt.Content += "\nwater:  " + gw.castle.WaterAmount + "/" + gw.castle.getWaterCapacity();
                     txt.Content += "\nstones: " + gw.castle.StoneAmount + "/" + gw.castle.getStoneCapacity();
                 }
 
-                soldiers_capacity.Text = gw.sCapacity + "/" + gw.collecting.capacity * gw.soldiers.Count();
+                //water_capacity.Text = gw.collecting.waterAmount + "/" + gw.fuzzyshit;
+                //stone_capacity.Text = gw.collecting.stoneAmount + "/" + gw.fuzzyshit;
 
 
 
